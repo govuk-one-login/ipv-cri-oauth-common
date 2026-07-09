542e487f618bf14daaf771718f740f8153e4ac7d
MarieseMikely <mariese.mikely@digital.cabinet-office.gov.uk>
2026-07-09 13:47:29 +0100

    chore: fix pre-commit for secret baseline and prettier for syncing changes

--- test-resources/headless-core-stub/utils/src/services/client-configuration.ts
+++ test-resources/headless-core-stub/utils/src/services/client-configuration.ts
@@ -3,13 +3,13 @@ import config from "./config";
 const { commonStackName } = config;
 
 export class ClientConfiguration {
-    public static async getConfig(clientId: string) {
-        const parameters = [
-            `/${commonStackName}/clients/${clientId}/jwtAuthentication/audience`,
-            `/${commonStackName}/clients/${clientId}/jwtAuthentication/issuer`,
-            `/${commonStackName}/clients/${clientId}/jwtAuthentication/redirectUri`,
-            `/test-resources/${clientId}/privateSigningKey`,
-        ];
-        return getParametersValues(parameters);
-    }
+  public static async getConfig(clientId: string) {
+    const parameters = [
+      `/${commonStackName}/clients/${clientId}/jwtAuthentication/audience`,
+      `/${commonStackName}/clients/${clientId}/jwtAuthentication/issuer`,
+      `/${commonStackName}/clients/${clientId}/jwtAuthentication/redirectUri`,
+      `/test-resources/${clientId}/privateSigningKey`,
+    ];
+    return getParametersValues(parameters);
+  }
 }
